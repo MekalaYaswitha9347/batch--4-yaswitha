d6ae3007d7175b568e584ac9c9361a5e75487d0b
MekalaYaswitha9347 <162985771+MekalaYaswitha9347@users.noreply.github.com>
2024-03-13 09:24:39 +0530

    Add files via upload

---
+++ yasta.m.py
@@ -0,0 +1,32 @@
+day 1
+Bargained characters
+controls and data structures->ABC language
+strings and its formats->from c
+Regular expressions->peart
+exception holding->Module3
+magic methods from->fortron
+
+what kind of language is a python
+python is a
+object oriented programming language
+Dynamically typed language
+scripting Language
+Interpreted Language
+compiled Language
+Functional programming Language
+  Flavours of python
+  (python-> writtenin c, most common implementation of python
+   jython-> java implementation of python
+   Iron python->implementation of python programming Language which is tightly integrated with NET
+   Brython->Browser python,run in browser
+   micro python->runs in micro controller
+   
+
+
+
+
+
+
+
+
+   
