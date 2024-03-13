8fcfbed3d9155017dccf78bd037c6d6f27615977
MekalaYaswitha9347 <162985771+MekalaYaswitha9347@users.noreply.github.com>
2024-03-13 15:59:48 +0530

    Add files via upload

---
+++ day 3.m.py
@@ -0,0 +1,191 @@
+## name=input("enter the name")
+## age=int(input("enter the age:"))
+## national=input("enter the nation:")
+
+## if age>=18 and nationality=="Indian":
+##    print("eligible for vote")
+## else:
+#    print("not eligible")
+ 
+f# from user and check if it is square or not.
+
+##length= int(input())
+## breadth= int(input())
+## if length==breadth:
+#    print("itsa square")
+## else:
+## print("its not square")
+
+
+#! Eg:4
+## python program to check whether the
+## given integer is a multiple of both 5 and 7
+
+
+n# char=int(input("enter the number:"))
+i# if n%5==0 and n%7==0:
+     print("yes")
+eelse:
+    print("no")
+
+
+!!Eg:5
+## write a program to accept the cost price of a bike and display the
+#road tax to be paid
+# according to the following criteria:
+
+#         cost price (in Rs)
+#         >100000
+#         >50000 and <=100000
+#        <=50000
+
+
+# price = int(input("enter the price:"))
+amount = 0
+# if price>=100000:
+    discount =price*(6/100)
+    value=price-discount
+    tax=value*(15/100)
+    total=value+tax
+else:
+    tax=price*(5/100)
+    total=price+tax
+ # print("The onroad cost of bike is: ",total)
+
+#! Eg:6
+#Accent The age of 4 people and display the oldest one
+# !---------> if elif else
+#Eg:1
+a# a=7
+b# b=9
+c# c=30
+## elif b>a and b>c:
+#   print("B is greater")
+## elif c>a and c>b:
+#    print("c is greater")
+
+## A school has following rules for grading system:
+## a. Below 25 -F
+## b. 25 to 45 -E
+## c. 45 to 50-D
+## d. 50 to 60-c
+## e. 60 to 80-B
+## if. Above 80-A
+## Ask user to enter marks and print the corresponding grade.
+
+m# mark=int(input("Enter mark:"))
+i# if  mark>=80 and mark<=100:
+ #   print("A")
+## elif mark>=60 and mark<=80:
+ #         print("B")
+## elif mark>=50 and mark<=60:
+##      print("C")
+## elif mark>=40 and mark<=50:
+##  (print("D")
+## else:
+## print("Fail")
+
+
+##  Eg:6
+## Acccept the age of 4 people and display
+     
+## ! ---> short hand if else
+## Eg:1
+a# a=90
+b# b=60
+## if a>b:
+ #  print("A")
+## else:
+## --> using short hand if else
+
+## 1.)statement inside the if condition have to be prsent at first  #2.)elif cannot be used in short hand if else  #3.)Always it have to end with else
+## print("A") if a>b else print ("B")
+
+## !code to check the given char is vowel or consonent
+ # char= input("Enter the char:"
+
+## ! ---> elif ladder using short hand if else
+ # Eg:1
+#  find greatest among 3 variables using short hand if else 
+# a= 8
+# b=5
+# c=9
+
+# print("A is greater") if a>b and a>c else print("B is greater")
+# if a<b and b<c else print ("c is greater")
+
+# ! --------------> looping
+             
+
+'     
+ 
+     
+
+
+
+     
+     
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
