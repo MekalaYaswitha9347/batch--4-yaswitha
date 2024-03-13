ea585dd9f8c6075f1fa9df4da3167cb852a5f489
MekalaYaswitha9347 <162985771+MekalaYaswitha9347@users.noreply.github.com>
2024-03-13 09:24:13 +0530

    Add files via upload

---
+++ yash.py
@@ -0,0 +1,172 @@
+#!// --> floor division
+
+#a=765433
+#b=19
+#print(a//b)
+
+#! ** --> used for power of a number
+#print(2**4) #--> 16
+
+#print(a)
+
+#a=30
+#a=-5
+#print(a)
+
+#! comparison-->==, >, <, !=, <=, >=
+#a=8
+#b=7
+#print(a>b)# True
+
+#a=9
+#b=5
+#print(a==b)
+
+#!Bitwise operator --> &, |, ^, ~, <<, >>
+a=7
+b=4
+print(a&b)
+
+#2^4 2^3 2^2 2^1 2^0
+#8    4    2     1
+
+#0   1    1    1     #--> 7
+#0    1   0     0     #-->4&
+#------------------------
+#0    1    0    0
+
+#! Logical --> used to compare the expressions
+#and, or , not
+a=6
+#print(a>3 and a<10)
+#print(a>7 or a<30)
+#(print(not(a>8 and a<10))
+
+#!Identify operator
+# are stored
+
+#is, is not
+a=8
+b=8
+#print(a is not b)
+
+# ! Membership operator
+# in, not in
+
+#11=[7,8,9,0,6,5]
+#num = 55
+#print(num  in 11)
+#print(num not in 11)
+
+#num =678
+#print(8 in num )# error
+
+#! conditional statement
+# if, elif, else
+
+#eg:1
+#a=6
+#if a:
+#print ("hello")
+
+Eg:2
+a=6
+if a>3:
+# print("hey")
+#else:
+#print("no")
+
+#Eg:2
+#a=6
+#if a>3:
+#    print("hello")
+
+#print("no")
+
+#eg:4
+#a=0
+#a=None
+#a=false
+#a=""
+#if a:
+#    print("yes")
+#else:
+#   print("no")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
